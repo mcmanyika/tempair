7a03c41b1dd44e17e00248e72c33f164ae656a4e
Micah <developer@heartfeltonline.org>
2024-06-25 05:27:19 -0500

    added image overlay

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -5,38 +5,68 @@ import Image from 'next/image';
 const ImageGallery = () => {
   const images = [
     {
-      src: '/images/1.jpeg',
-      alt: 'Image 1',
-      width: 800,
-      height: 600,
-    },
-    {
-      src: '/images/2.jpeg',
-      alt: 'Image 2',
-      width: 800,
-      height: 600,
-    },
-    {
-      src: '/images/3.jpeg',
-      alt: 'Image 3',
-      width: 800,
-      height: 600,
-    },
-    {
-      src: '/images/4.jpeg',
-      alt: 'Image 4',
-      width: 800,
-      height: 600,
-    },
-    {
-      src: '/images/5.jpeg',
-      alt: 'Image 5',
-      width: 800,
-      height: 600,
-    },
-    {
       src: '/images/6.jpeg',
-      alt: 'Image 6',
+      alt: '',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/12.png',
+      alt: 'Customised coldrooms',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/14.png',
+      alt: 'Brick & Motor Meat cold rooms',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/15.png',
+      alt: 'Abbatoir Drip rooms',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/16.png',
+      alt: 'Abbatoir  carcass conveyors',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/17.png',
+      alt: 'Movable blast freezers',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/18.png',
+      alt: 'Crane movable cold rooms',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/19.png',
+      alt: '',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/20.png',
+      alt: '',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/21.png',
+      alt: 'Industrial chillers maintenance',
+      width: 800,
+      height: 600,
+    },
+    {
+      src: '/images/22.png',
+      alt: 'Commercial Refrigeration specialists',
       width: 800,
       height: 600,
     },
@@ -61,14 +91,14 @@ const ImageGallery = () => {
       <div className="w-full py-12 px-4 sm:px-6 lg:px-8 mt-5 relative">
         <button
           onClick={scrollLeft}
-          className="absolute left-10 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white h-12 w-12 rounded-full z-10 hover:bg-gray-600 flex items-center justify-center"
+          className="absolute left-10 top-1/2 transform -translate-y-1/2 bg-gray-600 text-white h-12 w-12 rounded-full z-10 hover:bg-gray-400 flex items-center justify-center"
           aria-label="Scroll Left"
         >
           &lt;
         </button>
         <button
           onClick={scrollRight}
-          className="absolute right-10 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white h-12 w-12 rounded-full z-10 hover:bg-gray-600 flex items-center justify-center"
+          className="absolute right-10 top-1/2 transform -translate-y-1/2 bg-gray-600 text-white h-12 w-12 rounded-full z-10 hover:bg-gray-400 flex items-center justify-center"
           aria-label="Scroll Right"
         >
           &gt;
@@ -80,7 +110,7 @@ const ImageGallery = () => {
         >
           <div className="flex gap-2">
             {images.map((image, index) => (
-              <div key={index} className="relative w-[300px] h-[300px] flex-shrink-0">
+              <div key={index} className="relative w-[300px] h-[300px] flex-shrink-0 group">
                 <Image
                   src={image.src}
                   alt={image.alt}
@@ -89,6 +119,9 @@ const ImageGallery = () => {
                   quality={100}
                   className="rounded"
                 />
+                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center rounded">
+                  <span className="text-white text-lg capitalize text-center">{image.alt}</span>
+                </div>
               </div>
             ))}
           </div>
